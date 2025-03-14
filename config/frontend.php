698adf50874eb4be88c70887ccb3dc72e017f581
phanminhnhat <minhnhat5303@gmail.com>
2025-03-14 19:44:53 +0700

    Add files via upload

---
+++ config/frontend.php
@@ -0,0 +1,21 @@
+<?php
+    return [
+        'menu' => [
+            [
+                'name' => 'Trang chủ',
+                'route' => 'get.home'
+            ],
+            [
+                'name' => 'Khuyến mại hót',
+                'route' => 'get.product.promotion'
+            ],
+            [
+                'name' => 'Hệ thống cửa hàng',
+                'route' => 'get.static.shop_system'
+            ],
+            [
+                'name' => 'Sản phẩm bán chạy',
+                'route' => 'get.product.selling'
+            ],
+        ]
+    ];
